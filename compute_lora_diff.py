e35a5c9ce10cb26716623fcbd53be673bda52074
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-24 15:27:31 +0200

    diagnostics

--- compute_lora_diff.py
+++ compute_lora_diff.py
@@ -97,8 +97,8 @@ def main():
     hyper_lora_factory = partial(
         HyperLoRALinear,
         clip_size=768,
-        rank=4,
-        alpha=16,
+        rank=1,
+        alpha=8,
     )
     hyper_lora_layers = inject_hyper_lora(
         model.model.diffusion_model, ["attn2.to_k", "attn2.to_v"], hyper_lora_factory
