7ecf999927161e6ca8eb37ff40d1d651c885dc5f
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-10-04 08:04:09 +0200

    fixed notebook

--- scripts/compute_lora_diff.sh
+++ scripts/compute_lora_diff.sh
@@ -1,13 +1,13 @@
-EXP=class_name_airplane_lora_rank_1_lora_alpha_1e-05_iterations_500_lr_3e-05_start_guidance_9.0_negative_guidance_2.0_ddim_steps_50_hyper
-HYPER_LORA_DIR=/data/pwojcik/UnGuide/output/$EXP/models/
+EXP=lora_rank_1_iterations_400_lr_3e-05_start_guidance_9.0_negative_guidance_2.0_ddim_steps_50_hyper
+HYPER_LORA_DIR=/data/pwojcik/UnGuide/output_one/$EXP/models/
 
 
 python compute_lora_diff.py \
   --config configs/stable-diffusion/v1-inference.yaml \
   --ckpt models/sd-v1-4.ckpt \
   --lora "${HYPER_LORA_DIR}hyper_lora.pth" \
-  --prompts_json data/airplane.json \
-  --output_dir output/$EXP/ \
+  --prompts_json data/cat.json \
+  --output_dir output_one/$EXP/ \
   --device cuda:0 \
   --seed 42 \
   --ddim_steps 50 \
