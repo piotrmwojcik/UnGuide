f024d7f3f54070531b92cabcb8f854f36408625d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-10-19 13:15:07 +0200

    debug

--- hyper_lora.py
+++ hyper_lora.py
@@ -147,19 +147,19 @@ class HyperLoRALinear(nn.Module):
         #if clip_embedding.dim() == 2:
         #    clip_embedding = clip_embedding.mean(dim=0)
         #print(self.original(x).shape, self.hyper_lora(x, clip_embedding, parent.time_step).shape)
-        if parent.time_step == 0:
-            # Print once
-            if not hasattr(self, "_printed_original_trainables"):
-                total = 0
-                print("[original] Trainable parameters:")
-                for name, p in self.original.named_parameters():
-                    if p.requires_grad:
-                        print(f"  - {name} shape={tuple(p.shape)}  numel={p.numel():,}")
-                        total += p.numel()
-                print(f"[original] Total trainable params: {total:,}")
-                self._printed_original_trainables = True
-
-            return self.original(x)
+        # if parent.time_step == 0:
+        #     # Print once
+        #     if not hasattr(self, "_printed_original_trainables"):
+        #         total = 0
+        #         print("[original] Trainable parameters:")
+        #         for name, p in self.original.named_parameters():
+        #             if p.requires_grad:
+        #                 print(f"  - {name} shape={tuple(p.shape)}  numel={p.numel():,}")
+        #                 total += p.numel()
+        #         print(f"[original] Total trainable params: {total:,}")
+        #         self._printed_original_trainables = True
+        #
+        #     return self.original(x)
         return self.original(x) + self.hyper_lora(x, clip_embedding, parent.time_step)
 
 
