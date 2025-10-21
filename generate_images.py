eaf5a882e99e43d28b6523cf3bb79e513e717d52
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-10-21 11:23:41 +0200

    generate

--- generate_images.py
+++ generate_images.py
@@ -288,7 +288,7 @@ if __name__ == "__main__":
                     vec = torch.cat([t.reshape(-1).float() for t in tensors_flat_t_live], dim=0)
                 l2 = vec.norm(p=2).item()
 
-            print("prompt: ", f"||tensors_flat_t_live||_2 = {l2:.6f}")
+            print("prompt: ", prompt, f"||tensors_flat_t_live||_2 = {l2:.6f}")
 
             if l2 < 1.5:
                 w = 0
