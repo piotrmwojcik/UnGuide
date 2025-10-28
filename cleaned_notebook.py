4de649a55837d5d7e0337612c0683be0a345eb50
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-10-28 16:32:53 +0100

    cleaneded notebook

--- cleaned_notebook.py
+++ cleaned_notebook.py
@@ -495,9 +495,9 @@ if __name__ == "__main__":
         # make a PIL image from imgs[0] in [0,1]
         im_u8 = (imgs[0].clamp(0, 1) * 255).round().to(torch.uint8).cpu()
         im_pil = to_pil_image(im_u8)
-        print('!!! ', probs)
+        #print('!!! ', probs)
         # ---- save with BOTH cos (embed replacement) and clip similarity in filename ----
-        img_name = f"idx{i:03d}_seed{seed_i}_cos{cos_ok:.3f}_clip{probs:.3f}.png"
+        img_name = f"idx{i:03d}_seed{seed_i}_cos{cos_ok:.3f}_clip{probs[0]:.3f}.png"
         img_path = out_dir / img_name
         im_pil.save(img_path)
 
