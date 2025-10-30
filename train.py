2431e18da6a81c387e932a57f2e7403c30f85efe
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-10-30 15:28:55 +0100

    hyper lora management

--- train.py
+++ train.py
@@ -666,7 +666,7 @@ def main():
 
     for layer_name, layer in hyper_lora_layers:
         layer.set_parent_model(accelerator.unwrap_model(model))
-        base.hyper.add_hyperlora(layer_name, layer.hyper_lora)
+        accelerator.unwrap_model(model).hyper.add_hyperlora(layer_name, layer.hyper_lora)
 
         # Create sampler AFTER prepare so it uses the wrapped model
     sampler = DDIMSampler(base)
