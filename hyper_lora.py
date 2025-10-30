2252b9f3ab62861c59efc0f17027902332b57385
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-10-31 00:36:33 +0100

    hyper lora management

--- hyper_lora.py
+++ hyper_lora.py
@@ -144,7 +144,7 @@ class HyperLora(nn.Module):
 
     def get_lora_matrices(self, clip, t):
         B = clip.shape[0]
-        t_feats = self.make_t_feats(t, B, clip)
+        t_feats = self.make_t_feats(t, clip, B)
 
         emb = clip
         print('!!! ', emb.shape, t_feats.shape)
