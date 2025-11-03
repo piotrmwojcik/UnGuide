76215007d5513825c1488c38742665b0d11351f3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-03 13:15:32 +0100

    hyper lora management

--- train.py
+++ train.py
@@ -946,13 +946,12 @@ def main():
                     wandb.log({"sample (other) 3": wandb.Image(to_pil_image(im0), caption=caption)}, step=i)
 
                 idx = torch.randint(0, len(CIFAR100), (1,), device=accelerator.device).item()
-                sample_ = encode(f"a photo of the {CIFAR100[idx]}")
+                sample_ = clip_text_encoder(encode(f"a photo of the {CIFAR100[idx]}")).pooler_output.detach()
                 #sample_prompt = retain_tensors[idx].to(accelerator.device)
                 #with torch.no_grad():
                 #   sample_, _ = pooled_from_hidden_and_prompt(sample_prompt, target_text,
                 #                                                     tokenizer=tokenizer)
                 #   sample_ = sample_.unsqueeze(dim=0).to(accelerator.device)
-                print('!!!! ', sample_.shape)
                 base.hyper.set_context(sample_, torch.tensor([150]).to(accelerator.device))
                 base.hyper.compute_and_cache_loras(sample_, torch.tensor([150]).to(accelerator.device))
                 imgs = generate_and_save_sd_images(
