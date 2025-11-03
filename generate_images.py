0240e236adeff9b9923b0cc0784cd54464f911d6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-03 21:33:46 +0100

    hyper lora management

--- generate_images.py
+++ generate_images.py
@@ -336,8 +336,8 @@ if __name__ == "__main__":
                 empty_prompt = clip_text_encoder(inputs_empty).pooler_output.detach()
                 inputs_target = clip_text_encoder(inputs_empty).pooler_output.detach()
 
-                model_unl.hyper.set_context(t_prompt, torch.tensor([150]).to(accelerator.device))
-                model_unl.compute_and_cache_loras(cond_target, torch.tensor([150]).to(accelerator.device))
+                model_unl.hyper.set_context(t_prompt, torch.tensor([150]).to(model_unl.device))
+                model_unl.compute_and_cache_loras(cond_target, torch.tensor([150]).to(model_unl.device))
 
                 z = quick_sampler(cond, args.start_guidance, start_code, int(t_enc))
                 _ = model_unl.apply_model(z, t_enc_ddpm, cond)
