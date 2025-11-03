21f2747429fe432de4b1ad2d4ca8dd3d7c4330e5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-03 01:31:31 +0100

    hyper lora management

--- train.py
+++ train.py
@@ -966,10 +966,10 @@ def main():
                     sample_, _ = pooled_from_hidden_and_prompt(sample_prompt, target_text,
                                                                tokenizer=tokenizer)
                     sample_ = sample_.unsqueeze(dim=0).to(accelerator.device)
-                base.hyper.set_context(sample_, torch.tensor([150]).to(accelerator.device))
-                base.hyper.compute_and_cache_loras(sample_, torch.tensor([150]).to(accelerator.device))
+                #base.hyper.set_context(sample_, torch.tensor([150]).to(accelerator.device))
+                #base.hyper.compute_and_cache_loras(sample_, torch.tensor([150]).to(accelerator.device))
                 imgs = generate_and_save_sd_images(
-                    model=base,
+                    model=model_orig,
                     sampler=sampler,
                     prompt=None,
                     cond=sample_prompt,
