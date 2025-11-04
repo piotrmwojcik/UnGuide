8223d210218ac2d3e5ab36237c48e8d0373b773e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-04 09:05:37 +0100

    hyper lora management

--- train.py
+++ train.py
@@ -845,7 +845,7 @@ def main():
                     delta_live = tensors_flat_t1_live - tensors_flat_t_live
 
                     # e.g., MSE to the target step
-                    loss = 5.0 * criterion(delta_live, grads_flat_t)
+                    loss = 10.0 * criterion(delta_live, grads_flat_t)
                     loss_for_backward = loss / accelerator.gradient_accumulation_steps
                     loss_remove = loss.clone().detach()
                     print('loss remove ', loss_remove)
