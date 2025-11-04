c7b6aadf95e2ff5266647aa26257dfd97ea57b45
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-04 19:05:35 +0100

    hyper lora management

--- train.py
+++ train.py
@@ -841,7 +841,7 @@ def main():
                     _, current_timestep = accelerator.unwrap_model(model).hyper.get_context()
                     all_N = remove_all_prompts.shape[0]
                     ct = current_timestep.repeat(all_N, 1)
-                    print('!!!! ', remove_all_prompts.shape, ct.shape)
+                    print('!!!! ', remove_all_prompts.shape, current_timestep.shape, ct.shape)
                     base.hyper.set_context(remove_all_prompts, ct)
                     base.hyper.compute_and_cache_loras(
                         remove_all_prompts, current_timestep + 1
