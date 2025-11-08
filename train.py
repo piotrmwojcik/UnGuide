45836572bd5fc65537c79f064aab73845d61162a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-08 23:16:08 +0100

    deer notebook

--- train.py
+++ train.py
@@ -853,7 +853,7 @@ def main():
                     #ct = current_timestep.repeat(all_N)
                     base.hyper.set_context(remove_prompt, current_timestep)
                     base.hyper.compute_and_cache_loras(
-                        remove_all_prompts, ct
+                        remove_all_prompts, current_timestep
                     )
                     tensors_flat_t_live = base.hyper.flatten_cached_from_cache()
 
