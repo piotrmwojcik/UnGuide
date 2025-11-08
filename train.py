c99d751b1ffac2603c6ab5ef6ac4bfa8a122c29e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-08 13:06:37 +0100

    deer notebook

--- train.py
+++ train.py
@@ -671,7 +671,7 @@ def main():
     optimizer = torch.optim.Adam(trainable_params, lr=args.lr)
 
     scheduler = torch.optim.lr_scheduler.MultiStepLR(
-        optimizer, milestones=[300], gamma=0.5
+        optimizer, milestones=[30000], gamma=0.5
     )
 
     # Prepare for DDP / Mixed precision
