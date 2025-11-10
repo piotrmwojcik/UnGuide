4eb9803c3e950ae1accb7d551ddeeed3766b6243
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-10 11:34:44 +0100

    deer notebook

--- generate_images_nsfw.py
+++ generate_images_nsfw.py
@@ -190,13 +190,14 @@ if __name__ == "__main__":
             model.hyper.set_context(t_prompt, torch.tensor([150]).to(model.device))
             model.hyper.compute_and_cache_loras(t_prompt, torch.tensor([150]).to(model.device))
 
+
+            sampler = DDIMSampler(model=model)
             img = generate_images(
                 sampler=sampler, model=model,
                 start_code=start_code, prompt=prompt, device=model.device,
                 steps=args.steps
             )
 
-            sampler = DDIMSampler(model=model)
             img_np = img[0].cpu().permute(1, 2, 0).numpy()
             img_pil = to_pil_image((img_np * 255).astype(np.uint8))
 
