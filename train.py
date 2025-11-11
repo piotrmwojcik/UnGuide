3e8e1526504413d1963bcb658c24bc9fa440642f
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-11 01:38:56 +0100

    deer notebook

--- train.py
+++ train.py
@@ -774,7 +774,7 @@ def main():
                 #if False:
                 if 'neutral.json' in sample['file']:
                     with torch.no_grad():
-                        K = 30
+                        K = 10
                         sampled_list = random.sample(CIFAR100, K)
                         sampled_list = [f"A photo of the {cifar_100_category}." for cifar_100_category in sampled_list]
 
@@ -793,7 +793,7 @@ def main():
                         retain_prompts = torch.stack(processed, dim=0).to(base.device)
 
                     hyper = accelerator.unwrap_model(model).hyper
-                    batch_prompts = retain_prompts.repeat(5, 1)  # (10*K, D)
+                    batch_prompts = retain_prompts.repeat(50, 1)  # (10*K, D)
                     B = batch_prompts.shape[0]
                     perm = torch.randperm(B, device=batch_prompts.device)
                     batch_prompts = batch_prompts[perm]
