890870a08fcfef9196bfe7093cfb350feaf2ecf6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-13 01:13:15 +0100

    Fix

--- train_simple.py
+++ train_simple.py
@@ -613,16 +613,16 @@ def main():
                 #    batch_prompts,
                 #    torch.zeros(B, device=accelerator.device)
                 #)
-                tensors_flat_t0 = hyper.flatten_cached_from_cache()
+                #tensors_flat_t0 = hyper.flatten_cached_from_cache()
 
                 # Compute LoRAs at t=1, 2, 3, ... B
-                t_ = (torch.arange(B, device=accelerator.device) % B) + 1
-                hyper.compute_and_cache_loras(batch_prompts, t_)
-                tensors_flat_t1 = hyper.flatten_cached_from_cache()
+                #t_ = (torch.arange(B, device=accelerator.device) % B) + 1
+                #hyper.compute_and_cache_loras(batch_prompts, t_)
+                #tensors_flat_t1 = hyper.flatten_cached_from_cache()
 
                 # Loss: minimize change in LoRA weights across timesteps
-                delta = tensors_flat_t1 - tensors_flat_t0
-                loss_retain = retain_weight * delta.pow(2).mean()
+                #delta = tensors_flat_t1 - tensors_flat_t0
+                loss_retain = retain_weight * torch.tensor(0, device=accelerator.device)#delta.pow(2).mean()
             else:
                 loss_retain = torch.tensor(0.0, device=accelerator.device)
             accelerator.backward(loss_retain / accelerator.gradient_accumulation_steps)
