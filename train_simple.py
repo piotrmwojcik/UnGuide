0420b0865062190ad8e9cd626e700373194a70a9
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-14 00:21:52 +0100

    Fix

--- train_simple.py
+++ train_simple.py
@@ -598,7 +598,7 @@ def main():
             loss_remove = remove_weight * criterion(delta_live, grads_flat_t)
             accelerator.backward(loss_remove / accelerator.gradient_accumulation_steps)
 
-            if len(retain_embeddings) < 0:
+            if len(retain_embeddings) > 0:
                 # Sample multiple retain concepts
                 num_retain_samples = min(10, len(retain_embeddings))
                 sampled_retain_embs = random.sample(retain_embeddings, num_retain_samples)
@@ -630,7 +630,7 @@ def main():
             else:
                 loss_retain = torch.tensor(0.0, device=accelerator.device)
 
-            #accelerator.backward(loss_retain / accelerator.gradient_accumulation_steps)
+            accelerator.backward(loss_retain / accelerator.gradient_accumulation_steps)
 
             loss_remove_log = loss_remove.clone().detach()
             loss_retain_log = loss_retain.clone().detach()
