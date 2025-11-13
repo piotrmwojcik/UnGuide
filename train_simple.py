cbad913332e5263cefafd26eb1068c5900e1b462
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-13 19:38:18 +0100

    Fix

--- train_simple.py
+++ train_simple.py
@@ -595,7 +595,7 @@ def main():
             loss_remove = remove_weight * criterion(delta_live, grads_flat_t)
             accelerator.backward(loss_remove / accelerator.gradient_accumulation_steps)
 
-            if len(retain_embeddings) > 0:
+            if len(retain_embeddings) < 0:
                 # Sample multiple retain concepts
                 num_retain_samples = min(10, len(retain_embeddings))
                 sampled_retain_embs = random.sample(retain_embeddings, num_retain_samples)
