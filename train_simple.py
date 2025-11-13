1aa5d20139a46c511dff862f13e2f33e802b890e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-13 17:07:09 +0100

    Fix

--- train_simple.py
+++ train_simple.py
@@ -626,7 +626,7 @@ def main():
                 loss_retain = retain_weight * delta.pow(2).mean()
             else:
                 loss_retain = torch.tensor(0.0, device=accelerator.device)
-            #accelerator.backward(loss_retain / accelerator.gradient_accumulation_steps)
+            accelerator.backward(loss_retain / accelerator.gradient_accumulation_steps)
             loss_remove_log = loss_remove.clone().detach()
             loss_retain_log = loss_retain.clone().detach()
 
