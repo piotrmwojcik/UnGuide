9f4c395778a840acf480e8b1e370a7851c36ec2a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-14 08:42:56 +0100

    generation

--- generate_images_nsfw_cfg.py
+++ generate_images_nsfw_cfg.py
@@ -196,7 +196,7 @@ if __name__ == "__main__":
         
         exp_dirpath = os.path.join(args.output_dir, dirname)
         os.makedirs(os.path.join(exp_dirpath, "images"), exist_ok=True)
-        lora_path = os.path.join(exp_dirpath, "models", "hyper_lora.pth")
+        lora_path = os.path.join(exp_dirpath, "hyper_lora.pth")
         if not os.path.exists(lora_path):
             print(f"Skip {dirname} - hyper_lora.pth not found")
             continue
