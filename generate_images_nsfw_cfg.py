1ff6082e95953fc3af1ba9e371866449c338ba68
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-14 08:41:33 +0100

    generation

--- generate_images_nsfw_cfg.py
+++ generate_images_nsfw_cfg.py
@@ -178,8 +178,8 @@ if __name__ == "__main__":
     else:
         dirs = os.listdir(args.output_dir)
 
-    tokenizer = CLIPTokenizer.from_pretrained("models/clip-vit-large-patch14")
-    clip_text_encoder = CLIPTextModel.from_pretrained("models/clip-vit-large-patch14").to(args.device).eval()
+    tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+    clip_text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14").to(args.device).eval()
 
     for dirname in dirs:
         # Load prompts
