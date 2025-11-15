f6e2978e9e774c738b9176e9da18f460c91e8fd1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-16 00:31:37 +0100

    generation

--- generate_images_nsfw_cfg.py
+++ generate_images_nsfw_cfg.py
@@ -208,7 +208,7 @@ if __name__ == "__main__":
             print(f"Skip {dirname} - already processed")
             continue
         print(f"Processing experiment: {dirname}.", flush=True)
-        print("images", len(os.listdir(os.path.join(exp_dirpath, "images"))), flush=True)
+        print("images", len(os.listdir(os.path.join(exp_dirpath, args.save_folder))), flush=True)
 
         # Load and prepare models - model_orig for unconditional, model for conditional
         model_orig = load_model_from_config(args.config, args.ckpt, args.device)
