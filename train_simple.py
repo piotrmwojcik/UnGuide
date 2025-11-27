44fd7c6e906b0badece2916ebef5e9605d7ace51
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-27 01:16:37 +0100

    meru clip

--- train_simple.py
+++ train_simple.py
@@ -371,7 +371,7 @@ def main():
     meru_checkpoint_path = config.get('meru_checkpoint_path', None)
     tokenizer_MERU = Tokenizer()
     _C_TRAIN = LazyConfig.load(meru_train_config)
-    model = LazyFactory.build_model(_C_TRAIN, accelerator.device).eval()
+    merum_model = LazyFactory.build_model(_C_TRAIN, accelerator.device).eval()
     CheckpointManager(model=model).load(meru_checkpoint_path)
 
     def encode_clip(text: str):
@@ -400,7 +400,7 @@ def main():
     for concept in target_concepts:
         inputs = encode_meru(concept)
         with torch.no_grad():
-            emb = model.encode_text(inputs, project=True)[0].detach()
+            emb = meru_model.encode_text(inputs, project=True)[0].detach()
         target_embeddings.append(emb)
     
     # Mapping concept embeddings (retain)
@@ -408,7 +408,7 @@ def main():
     for concept in mapping_concept:
         inputs = encode_meru(concept)
         with torch.no_grad():
-            emb = model.encode_text(inputs, project=True)[0].detach()
+            emb = meru_model.encode_text(inputs, project=True)[0].detach()
         mapping_embeddings.append(emb)
     
     # Retain prompts - load from CSV file with 'prompt' column
@@ -458,7 +458,7 @@ def main():
                 for prompt in tqdm(retain_prompts, desc="Creating retain embeddings"):
                     inputs = encode_meru(prompt)
                     with torch.no_grad():
-                        emb = model.encode_text(inputs, project=True)[0].detach()
+                        emb = meru_model.encode_text(inputs, project=True)[0].detach()
                     retain_embeddings.append(emb.squeeze().cpu())  # Store on CPU for caching
                 
                 print(f"Caching retain embeddings to: {cache_path}")
@@ -535,7 +535,7 @@ def main():
                 # Recompute target_emb with the same augmentation
                 inputs_aug = encode_meru(target_text_augmented)
                 with torch.no_grad():
-                    target_emb = model.encode_text(inputs_aug, project=True)[0].detach()
+                    target_emb = meru_model.encode_text(inputs_aug, project=True)[0].detach()
 
             else:
 
@@ -673,7 +673,7 @@ def main():
                 # Encode the diagnostic prompt
                 inputs_diag = encode_meru(diag_prompt)
                 with torch.no_grad():
-                    diag_emb = model.encode_text(inputs_adiag, project=True)[0].detach()
+                    diag_emb = meru_model.encode_text(inputs_diag, project=True)[0].detach()
                 
                 base.hyper.set_context(diag_emb, torch.tensor([hyper_train_steps], device=accelerator.device))
                 base.hyper.compute_and_cache_loras(diag_emb, torch.tensor([hyper_train_steps], device=accelerator.device))
