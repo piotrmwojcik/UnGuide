b2cda95eb0a6cfe182e789ce9589636b23c31c68
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-11 23:28:34 +0100

    text embeddings

--- utils/esd_utils.py
+++ utils/esd_utils.py
@@ -72,6 +72,7 @@ def latent_sample(transformer, scheduler, batch_size, num_channels_latents, heig
     prompt_embeds = prompt_embeds.bfloat16()
     text_ids = text_ids.bfloat16()
 
+    attn_map_lst = []
     # Denoising loop
     for i, t in enumerate(timesteps):
         # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
@@ -79,7 +80,7 @@ def latent_sample(transformer, scheduler, batch_size, num_channels_latents, heig
 
         # print(latents.shape, timestep)
         # self.transformer.config.guidance_embeds False => guidance = None
-        noise_pred = transformer(
+        noise_pred, attn_maps = transformer(
             hidden_states=latents,
             timestep=timestep / 1000,
             guidance=guidance,
@@ -93,6 +94,11 @@ def latent_sample(transformer, scheduler, batch_size, num_channels_latents, heig
         # compute the previous noisy sample x_t -> x_t-1
         latents = scheduler.step(noise_pred, t, latents, return_dict=False)[0]
 
+        attn_map_lst.append(attn_maps)
+
+    if return_attn:
+        return latents, latent_image_ids, attn_map_lst
+    else:
         return latents, latent_image_ids
 
 
