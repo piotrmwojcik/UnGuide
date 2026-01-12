9f50151a64877fd1ab3459a8a6c7fda5142cb70a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-12 23:35:12 +0100

    text embeddings

--- generate_bare_flux.py
+++ generate_bare_flux.py
@@ -93,6 +93,20 @@ def import_model_class_from_model_name_or_path(
     else:
         raise ValueError(f"{model_class} is not supported.")
 
+
+def calculate_shift(
+    image_seq_len,
+    base_seq_len: int = 256,
+    max_seq_len: int = 4096,
+    base_shift: float = 0.5,
+    max_shift: float = 1.15,
+):
+    m = (max_shift - base_shift) / (max_seq_len - base_seq_len)
+    b = base_shift - m * base_seq_len
+    mu = image_seq_len * m + b
+    return mu
+
+
 @torch.no_grad()
 def inference_latent_sample(transformer, scheduler, batch_size, num_channels_latents, height, width, prompt_embeds,
                   pooled_prompt_embeds, text_ids, guidance, timesteps, vae_scale_factor, latents=None):
