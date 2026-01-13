3924b999b834e5f96ff11b454baa2521cb088cfb
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-13 15:36:56 +0100

    text embeddings

--- generate_base_flux.py
+++ generate_base_flux.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     parser.add_argument("--image_size", type=int, default=512)
     parser.add_argument("--num_inference_steps", type=int, default=28)
     parser.add_argument("--nudity", type=bool, default=True)
-    parser.add_argument("--guidance_scale", type=float, default=7.5)
+    parser.add_argument("--guidance_scale", type=float, default=3.0)
     parser.add_argument("--n_images", type=int, default=None)
     parser.add_argument("--device", type=str, default="cuda")
     args = parser.parse_args()
@@ -92,6 +92,7 @@ if __name__ == "__main__":
         start = time.time()
         image = pipe(
             prompt=prompt,
+            guidance_scale=args.guidance_scale,
             num_inference_steps=args.num_inference_steps,
             height=args.image_size,
             width=args.image_size,
