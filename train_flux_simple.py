0a4cc699bc09b6dba2ac74cebad58c87664140bc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-14 22:12:52 +0100

    merge

--- train_flux_simple.py
+++ train_flux_simple.py
@@ -759,17 +759,17 @@ def main():
                                                         model_input.shape[1],
                                                         512,
                                                         512,
-                                                        emb_p.to(transformer.device),
-                                                        pooled_emb_p.to(transformer.device),
-                                                        text_ids_p.to(transformer.device),
+                                                        emb_p.to(accelerator.device),
+                                                        pooled_emb_p.to(accelerator.device),
+                                                        text_ids_p.to(accelerator.device),
                                                         start_guidance,
                                                         int(ddim_steps),
                                                         vae_scale_factor)
                     e_0 = predict_noise(model, z, emb_0, pooled_emb_0, text_ids_0, latent_image_ids,
-                                        guidance=start_guidance, timesteps=t_enc_ddpm.to(transformer.device),
+                                        guidance=start_guidance, timesteps=t_enc_ddpm.to(accelerator.device),
                                         CPU_only=True)
                     e_p = predict_noise(model, z, emb_p, pooled_emb_p, text_ids_p, latent_image_ids,
-                                        guidance=start_guidance, timesteps=t_enc_ddpm.to(transformer.device),
+                                        guidance=start_guidance, timesteps=t_enc_ddpm.to(accelerator.device),
                                         CPU_only=True)
 
             base.hyper.set_context(target_emb.to(dtype=weight_dtype), torch.tensor([rtimestep], dtype=weight_dtype, device=accelerator.device))
