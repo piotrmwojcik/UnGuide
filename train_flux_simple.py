c5b379594e4a1aaaa727a23ae82f9bc5a28bc0df
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-22 10:25:33 +0100

    pca

--- train_flux_simple.py
+++ train_flux_simple.py
@@ -1301,7 +1301,7 @@ def main():
                                                             pooled_emb_p.to(accelerator.device),
                                                             text_ids_p.to(accelerator.device),
                                                             start_guidance,
-                                                            int(t_enc))
+                                                            int(ddim_steps))
                 with base.hyper.no_lora():
                     e_0 = predict_noise(
                         model, z, emb_0.to(dtype=weight_dtype), pooled_emb_0.to(dtype=weight_dtype), text_ids_0, latent_image_ids,
