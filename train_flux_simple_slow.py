e5c8e4d8237fd367132c39bd8b6d912919c2c31a
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-25 12:10:35 +0100

    celeb maksym

--- train_flux_simple_slow.py
+++ train_flux_simple_slow.py
@@ -1106,7 +1106,7 @@ def main():
         text_encoder_2=text_encoder_two,
         tokenizer_2=tokenizer_two,
     )
-    cpu_offload(diag_pipe, device="cuda")
+    cpu_offload(diag_pipe, device=accelerator.device)
     #diag_pipe.to(accelerator.device)
 
     #diag_pipe.transformer.to(device=device, dtype=weight_dtype).eval()
