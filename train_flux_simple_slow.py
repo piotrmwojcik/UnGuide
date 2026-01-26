1ec46305c9e4ecaa80b55db9003ea94ddc9a1636
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-26 17:22:22 +0100

    celeb maksym

--- train_flux_simple_slow.py
+++ train_flux_simple_slow.py
@@ -849,7 +849,7 @@ def main():
 
     # Flux's pooled_prompt_embeds is always 768-dim (from built-in CLIP text_encoder_one)
     clip_size = 768
-    target_modules = ["attn.add_v_proj", "attn.to_out.0"]
+    target_modules = ["attn.add_v_proj", "attn.to_v", "attn.to_out.0"]
 
     hyper_lora_factory = partial(
         HyperLoRALinear,
