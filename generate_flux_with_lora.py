44ef4314c3e290191cb00246a739fa6c42ee4b50
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-27 13:26:46 +0100

    celeb maksym

--- generate_flux_with_lora.py
+++ generate_flux_with_lora.py
@@ -160,7 +160,7 @@ if __name__ == "__main__":
     model_wrapper.hyper = HypernetworkManager()
 
     clip_size = 768 if args.use_pooler else 512
-    target_modules = ["attn.add_k_proj", "attn.add_q_proj", "attn.add_v_proj"]
+    target_modules = ["attn.add_k_proj", "attn.add_q_proj"]
 
 
     hyper_lora_factory = partial(
