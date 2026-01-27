8bbe6eab890d55778a3122c2f1648b6bda758ca4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-01-27 02:06:32 +0100

    celeb maksym

--- train_flux_simple_slow.py
+++ train_flux_simple_slow.py
@@ -1325,7 +1325,7 @@ def main():
                                                             text_ids_p.to(accelerator.device),
                                                             start_guidance,
                                                             int(ddim_steps),
-                                                            stop_at_step=int(ddim_steps))
+                                                            stop_at_step=int(t_ddpm.item()))
                 with base.hyper.no_lora():
                     e_0 = predict_noise(
                         model, z, emb_0.to(dtype=weight_dtype), pooled_emb_0.to(dtype=weight_dtype), text_ids_0, latent_image_ids,
