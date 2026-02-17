91c23b140dfaf4db8cdd11b1ca10e940863dc1c4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-02-17 13:48:41 +0100

    seed

--- generate_base_flux.py
+++ generate_base_flux.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
         start = time.time()
         image = pipe(
             prompt=prompt,
-            guidance_scale=3.5,
+            #guidance_scale=3.5,
             num_inference_steps=args.num_inference_steps,
             height=args.image_size,
             width=args.image_size,
