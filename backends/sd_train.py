e0b77bcfca74695f99f7008828202f04eac979c2
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2026-02-26 15:28:59 +0100

    seed

--- backends/sd_train.py
+++ backends/sd_train.py
@@ -578,7 +578,7 @@ def main():
                     batch_prompts = batch_retain_embs[perm]
 
                     # Compute LoRAs at t=0
-                    dtype = next(hyper.parameters()).dtype  # hyper’s param dtype (bf16 if you casted it)
+                    weight_dtype = next(hyper.parameters()).dtype  # hyper’s param dtype (bf16 if you casted it)
 
                     hyper.compute_and_cache_loras(
                         batch_prompts.to(dtype=dtype).to(dtype=weight_dtype),
